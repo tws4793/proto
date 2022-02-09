cb4dc968d76c0df7f88077422acbe28e48f41d12
Teo Wei Shen <7098637+tws4793@users.noreply.github.com>
2022-02-09 17:20:06 +0800

    configure alpine

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import './style.css'
+import Alpine from 'alpinejs'
+
+window.Alpine = Alpine
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
@@ -6,3 +9,5 @@ app.innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
+
+Alpine.start()
